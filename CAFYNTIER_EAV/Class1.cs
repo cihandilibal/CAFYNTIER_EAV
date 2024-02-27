61a344911b072ba13be9e43d6f216c0835e58b8a
cihan dilibal <dilibalcihan@gmail.com>
2024-02-27 22:04:33 +0300

    Add project files.

---
+++ CAFYNTIER_EAV/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAFYNTIER_EAV
+{
+    public class Class1
+    {
+    }
+}
